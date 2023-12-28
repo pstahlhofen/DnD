e281dd2ec6c0b33d2e52e220891de8372087ef26
Paul Stahlhofen <pstahlhofen@techfak.uni-bielefeld.de>
2023-12-28 14:38:37 +0100

    Initial Commit

---
+++ library_labyrinth.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+edgelist = pd.read_csv('edgelist.csv', header=None).to_numpy()
+G = nx.from_edgelist(edgelist).to_undirected()
+pos_f = lambda n: ((n-1)%5 , 5-((n-1)//5))
+pos = {n: pos_f(n) for n in G.nodes}
+nx.draw_networkx(G, pos)
+plt.show()
